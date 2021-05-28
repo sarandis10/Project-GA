fc6798cb849dd106fa6f8990e15f4f468656e420
sarandis <sarandis10@hotmail.com>
2021-05-28 11:06:39 +0100

    trying to fix the stretch

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,25 @@
 function init() {
     
-const theGrid=document.querySelector()
+const theGrid=document.querySelector(".grid-screen")
+console.log(theGrid)
+const width=20
+const numberCells=width*(width)-38
+// console.log(numberCells)
+const cellsArray=[]
+
+const spaceshipClass="spaceShip"
 
 
-
-
-
+//The Grid
+function createGrid(){
+  for (let i=1; i<numberCells; i++){
+    const cell= document.createElement("div")
+    cell.innerText = i
+    theGrid.appendChild(cell)
+    cellsArray.push(cell)
   }
+}
+createGrid()
+
+}
   window.addEventListener('DOMContentLoaded', init)

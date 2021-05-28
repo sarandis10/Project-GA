7db8cb08d846ac20469a15f443ffa06ff508db06
sarandis <sarandis10@hotmail.com>
2021-05-28 11:23:59 +0100

    grid issues done

--- scripts/app.js
+++ scripts/app.js
@@ -3,11 +3,13 @@ function init() {
 const theGrid=document.querySelector(".grid-screen")
 console.log(theGrid)
 const width=20
-const numberCells=width*(width)-38
+const numberCells=width*(width)-39
 // console.log(numberCells)
 const cellsArray=[]
 
 const spaceshipClass="spaceShip"
+
+// const shipStartPosition
 
 
 //The Grid

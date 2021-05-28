aa6a5e71ce04549014dc1988f652c66ca7128e4e
sarandis <sarandis10@hotmail.com>
2021-05-28 16:55:40 +0100

    enemy moves

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 function init() {
-  let score=0
+  let score = 0
   // const firstName = (window.prompt("Hello There what is your name?"))
   // let lifes=Number(window.prompt(`hello ${firstName} how many lifes do you want to begin with?`))
-  
+
   // if (lifes>5){
   //   lifes=Number(window.prompt(`${firstName} dont you think you asking for too many lifes choose again but less that 5 this time!?`))
   // }
@@ -10,7 +10,7 @@ function init() {
   // if (lifes>5){
   //   lifes=Number(window.prompt(`${firstName} dont you think you asking for too many lifes choose again but less that 5 this time!?`))
   // }
-  let lifes= 2
+  let lifes = 2
   console.log(lifes)
 
   const theGrid = document.querySelector(".grid-screen")
@@ -20,24 +20,25 @@ function init() {
   console.log("the number of cells:", numberCells)
   const cellsArray = []
   console.log("my array with all divs:", cellsArray)
-  //? spaceship stuff
-  const spaceshipClass = 'spaceShip'
-  const shipStartPosition = numberCells-11
-  let shipCurrentPosition = numberCells-11
 
-  console.log("ship starting position cell: ", shipStartPosition)
-  console.log("ship current position cell: ", shipCurrentPosition)
-  //! this is enemy stuff
-  const enemyClass ="enemy" 
-  const enemyStartPosition=7
-  let enemyCurrentPosition=7
+
+  // spaceship stuff
+  const spaceshipClass = 'spaceShip'
+  const shipStartPosition = numberCells - 11
+  let shipCurrentPosition = numberCells - 11
 
   
+  //! this is enemy stuff
+  const enemyClass = "enemy"
+  const enemyStartPosition = 7
+  let enemyCurrentPosition = 7
 
 
 
 
-  //The Grid
+
+
+  //?The Grid
   function createGrid() {
     for (let i = 1; i < numberCells; i++) {
       const cell = document.createElement("div")
@@ -49,17 +50,29 @@ function init() {
     addEnemy(enemyStartPosition)
   }
 
-  //Add the ship to the grid
 
+
+  //Add the ship to the grid
   function addSpaceShip(position) {
     console.log('position of the spaceship :', position)
     cellsArray[position].classList.add(spaceshipClass)
   }
 
-  //remoce spaship from the grid
-  function removeSpaceShip(position){
+  //! add enemy to the grid!
+  function addEnemy(position) {
+    console.log('position of the enemy :', position)
+    cellsArray[position].classList.add(enemyClass)
+  }
+
+  //remove spacehip to the grid
+  function removeSpaceShip(position) {
     console.log("remove works")
     cellsArray[position].classList.remove(spaceshipClass)
+  }
+
+  //!remove enemy from the grid
+  function removeEnemy(position) {
+    cellsArray[position].classList.remove(enemyClass)
   }
 
   //movement logic 
@@ -68,43 +81,39 @@ function init() {
     console.log(key)
     removeSpaceShip(shipCurrentPosition)
 
-    if (key==39 && shipCurrentPosition<numberCells-2){
+    if (key == 39 && shipCurrentPosition < numberCells - 2) {
       shipCurrentPosition++
     }
-    if (key==37 && shipCurrentPosition>0){
+    if (key == 37 && shipCurrentPosition > 0) {
       shipCurrentPosition--
     }
-    if (key==38 && shipCurrentPosition >= width){
-      shipCurrentPosition=shipCurrentPosition-width
+    if (key == 38 && shipCurrentPosition >= width) {
+      shipCurrentPosition = shipCurrentPosition - width
     }
-    if (key==40 && shipCurrentPosition<341){
-      shipCurrentPosition=shipCurrentPosition+width
+    if (key == 40 && shipCurrentPosition < 341) {
+      shipCurrentPosition = shipCurrentPosition + width
     }
+    
     addSpaceShip(shipCurrentPosition)
   }
 
 
   document.addEventListener('keyup', handleKeyUp)
   createGrid()
-
-
-  //! add enemy to the grid!
-function addEnemy(position){
-  cellsArray[position].classList.add(enemyClass)
-  console.log("enemy position: ",enemyStartPosition)
   enemymoves(enemyStartPosition)
-}
-
-function removeEnemy(position){
-  console.log("remove ENEMY works")
-  cellsArray[position].classList.remove(enemyClass)
-}
-
-function enemymoves(position){
-  // removeEnemy(position)
-  // enemyCurrentPosition++
-  // addEnemy(enemyCurrentPosition)
-}
+  //! move enemy
+  function enemymoves(position) {
+    addEnemy(position)
+      
+    const timer = setInterval(() => {
+      removeEnemy(enemyCurrentPosition)
+      console.log("enemy pos",position)
+      enemyCurrentPosition=enemyCurrentPosition+1
+      console.log("enemy pos after itt",enemyCurrentPosition)
+      addEnemy(enemyCurrentPosition)
+    }, 500)
+    
+  }
 
 
 }
